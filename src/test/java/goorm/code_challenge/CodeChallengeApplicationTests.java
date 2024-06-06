580133e2f8c46a64dad1e82a3b6e11ebf3383ff7
SeongJunChun <csj991210@naver.com>
2024-06-06 22:12:55 +0900

    Initial commit

---
+++ src/test/java/goorm/code_challenge/CodeChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package goorm.code_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
