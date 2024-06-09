461d68b9a51e17e50cde8b89d98529856b482472
SeongJunChun <csj991210@naver.com>
2024-06-09 22:39:11 +0900

    FEAT: CORS 설정 완료

--- src/main/java/goorm/code_challenge/jwt/config/SecurityConfig.java
+++ src/main/java/goorm/code_challenge/jwt/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package goorm.code_challenge.jwt.config;
 
+import java.util.Collections;
+
 import goorm.code_challenge.jwt.application.JWTUtil;
 import goorm.code_challenge.jwt.filter.JWTFilter;
 import goorm.code_challenge.jwt.filter.LoginFilter;
+import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +17,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 /**
  * 인증은 CustomJsonUsernamePasswordAuthenticationFilter에서 authenticate()로 인증된 사용자로 처리
@@ -50,6 +55,24 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+		http
+			.cors((cors)->cors
+				.configurationSource(new CorsConfigurationSource() {
+					@Override
+					public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+						CorsConfiguration configuration = new CorsConfiguration();
+
+						configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+						configuration.setAllowedMethods(Collections.singletonList("*"));
+						configuration.setAllowCredentials(true);
+						configuration.setAllowedHeaders(Collections.singletonList("*"));
+						configuration.setMaxAge(3600L);
+
+						configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+						return configuration;
+					}
+				}));
 
 		http
 			.csrf((auth) -> auth.disable());
