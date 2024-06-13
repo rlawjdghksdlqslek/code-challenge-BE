0e9f3ba5082a3c681f68961fc76e244eb66c5521
SeongJunChun <146021359+SeongJunChun@users.noreply.github.com>
2024-06-13 13:48:18 +0900

    FEAT : DinD 설정 테스트

--- src/main/java/goorm/code_challenge/ide/utils/DockerCommand.java
+++ src/main/java/goorm/code_challenge/ide/utils/DockerCommand.java
@@ -19,15 +19,16 @@ public class DockerCommand {
 	// }
 	//배포 버전
 	public static List<String> javaCommand(File directory) {
-		String containerDirectory = "/app";
+		//String hostDirectory = "/tmp";
 		return Arrays.asList(
-			"docker", "run", "-i", "--rm", "--privileged", "-d", "docker:dind",
+			"docker", "exec", "-it", "java-docker",
 			"docker", "run", "-i", "--rm",
 			"-v", "/tmp" + ":/app",
 			"openjdk:17",
 			"java", "-cp", "/app/" + directory.toString().replace("/tmp", ""), "Main"
 		);
 	}
+
 
 	public static List<String> javaScriptCommand(File directory) {
 		return Arrays.asList(
@@ -63,14 +64,17 @@ public class DockerCommand {
 	//베포버전
 	public static List<String> compileCommand(File sourceFile, String tempDirPath) {
 		return Arrays.asList(
-			"docker", "run", "-i", "--rm", "--privileged", "-d", "docker:dind",
+			"docker", "exec", "-it", "java-docker",
 			"docker", "run", "--rm",
-			"-v", "/tmp" + "/" + tempDirPath.toString().replace("/tmp", "") + ":/app",
+			"-v", "/tmp" + "/" +
+				tempDirPath.toString().replace("/tmp", "") +
+				":/app",
 			"-w", "/app",
 			"openjdk:17",
 			"javac", sourceFile.getName()
 		);
 	}
+
 
 
 }
