a44960d455fc7b79ba5f3d9e4a69faecb6ed6a90
SeongJunChun <146021359+SeongJunChun@users.noreply.github.com>
2024-06-14 18:12:33 +0900

    feat : Config 설정 변경

--- src/main/java/goorm/code_challenge/jwt/config/SecurityConfig.java
+++ src/main/java/goorm/code_challenge/jwt/config/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
 					public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
 						CorsConfiguration configuration = new CorsConfiguration();
 
-						configuration.setAllowedOrigins(Collections.singletonList("https://localhost:3000"));
+						configuration.setAllowedOrigins(Arrays.asList("https://localhost:3000","https://localhost:3000/code-editor"));
 						configuration.setAllowedMethods(Collections.singletonList("*"));
 						configuration.setAllowCredentials(true);
 						configuration.setAllowedHeaders(Collections.singletonList("*"));
