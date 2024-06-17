c65c02f66a1bbdb2f6bbd679adeea5532537bd77
SeongJunChun <146021359+SeongJunChun@users.noreply.github.com>
2024-06-17 11:46:53 +0900

    FEAT : url 매핑 수정

--- src/main/java/goorm/code_challenge/room/api/RoomController.java
+++ src/main/java/goorm/code_challenge/room/api/RoomController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/rooms")
+@RequestMapping("/api/rooms")
 @RequiredArgsConstructor
 public class RoomController {
 
