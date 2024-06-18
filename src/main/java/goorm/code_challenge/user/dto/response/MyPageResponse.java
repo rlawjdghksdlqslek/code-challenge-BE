dbbc05981b39d4a57b5f5481a999fbf118113b5f
SeongJunChun <146021359+SeongJunChun@users.noreply.github.com>
2024-06-18 16:34:35 +0900

    FEAT : 마이페이지 레벨 경험치 표현 추가

--- src/main/java/goorm/code_challenge/user/dto/response/MyPageResponse.java
+++ src/main/java/goorm/code_challenge/user/dto/response/MyPageResponse.java
@@ -10,12 +10,16 @@ public class MyPageResponse {
 	private String userLoginId;
 	private String userName;
 	private String profileImage;
+	private Integer level;
+	private Float extraExp;
 	private List<MyCodes> myCodes;
 
 	public MyPageResponse(User user,List<MyCodes> myCodes) {
 		this.userLoginId=user.getLoginId();
 		this.userName=user.getName();
 		this.profileImage=user.getProfileImage();
+		this.level=user.getLevel();
+		this.extraExp=user.getExtraExpPoints();
 		this.myCodes=myCodes;
 	}
 }
